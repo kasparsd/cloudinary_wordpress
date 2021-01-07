cba29636ccb1c04cbf45ea598af78b7ab0da20d2
David Cramer <david@digilab.co.za>
2021-01-07 14:46:50 +0200

    update handlers and styling

--- js/src/components/notices.js
+++ js/src/components/notices.js
@@ -3,30 +3,40 @@ const Notices = {
 	_init() {
 		const self = this;
 		if ( typeof CLDIS !== 'undefined' ) {
-			const notices = document.getElementsByClassName( 'cld-notice' );
+			const notices = document.getElementsByClassName( 'cld-notice-box' );
 			[ ...notices ].forEach( ( notice ) => {
-				notice.addEventListener( 'click', ( ev ) => {
-					// WordPress has an onClick that cannot be unbound.
-					// So, we have the click on our Notice, and act on the
-					// button as a target.
-					if ( 'notice-dismiss' === ev.target.className ) {
+				notice.style.height = notice.offsetHeight + 'px';
+				const dismiss = notice.getElementsByClassName(
+					'notice-dismiss'
+				);
+				if ( dismiss.length ) {
+					dismiss[ 0 ].addEventListener( 'click', ( ev ) => {
+						ev.preventDefault();
 						self._dismiss( notice );
-					}
-				} );
+					} );
+				}
 			} );
 		}
 	},
 	_dismiss( notice ) {
 		const token = notice.dataset.dismiss;
-		const duration = notice.dataset.duration;
-		wp.ajax.send( {
-			url: CLDIS.url,
-			data: {
-				token,
-				duration,
-				_wpnonce: CLDIS.nonce,
-			},
-		} );
+		const duration = parseInt( notice.dataset.duration );
+		notice.classList.add( 'dismissed' );
+		notice.style.height = '0px';
+		if ( 0 < duration ) {
+			wp.ajax
+				.send( {
+					url: CLDIS.url,
+					data: {
+						token,
+						duration,
+						_wpnonce: CLDIS.nonce,
+					},
+				} )
+				.always( function () {
+					notice.remove();
+				} );
+		}
 	},
 };
 
